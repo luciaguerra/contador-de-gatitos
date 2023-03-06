7210e69fd50cca85caa8c49dfee04940f64432c9
Lucía <luciaguerra441@gmail.com>
2023-03-06 03:10:49 -0300

    The saved values get pushed into an array and saved into Local Storage

--- js/codigo.js
+++ js/codigo.js
@@ -1,10 +1,12 @@
 let count = 0;
+let gatos = [];
 
 const saveBtn = document.getElementById("save-btn");
 const incrementBtn = document.getElementById("increment-btn");
 const saveEl = document.getElementById("save-el");
-const countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el");
 const welcomeEl = document.getElementById("welcome-el");
+const gatosFromLocalStorage = localStorage.getItem("gatos");
 
 let greeting = "¡Hola, michilover! Contá cuántos michis ves a lo largo de tu día";
 
@@ -13,10 +15,14 @@ welcomeEl.innerText = greeting + " 😸";
 incrementBtn.addEventListener("click", function(){
     count +=1;
     countEl.innerText = count;
+    console.log(gatos);
 })
 
 saveBtn.addEventListener("click", function(){
+    gatos.push(count);
+    localStorage.setItem("gatos", JSON.stringify(gatos));
     saveEl.innerText += count + " - ";
     count = 0;
     countEl.innerText = 0;
 })
+
