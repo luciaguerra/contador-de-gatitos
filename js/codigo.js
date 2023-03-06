912cd06833aff32580bc0ae429fe90306f103644
Lucía <luciaguerra441@gmail.com>
2023-03-06 01:17:29 -0300

    Changed functions into event listeners

--- js/codigo.js
+++ js/codigo.js
@@ -1,23 +1,22 @@
-let welcomeEl = document.getElementById("welcome-el");
+let count = 0;
+
+const saveBtn = document.getElementById("save-btn");
+const incrementBtn = document.getElementById("increment-btn");
+const saveEl = document.getElementById("save-el");
+const countEl = document.getElementById("count-el");
+const welcomeEl = document.getElementById("welcome-el");
 
 let greeting = "¡Hola, michilover! Contá cuántos michis ves a lo largo de tu día";
 
-welcomeEl.innerText = greeting;
+welcomeEl.innerText = greeting + " 😸";
 
-welcomeEl.innerText += " 😸";
-
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
-
-let count = 0;
-
-function increment() {
+incrementBtn.addEventListener("click", function(){
     count +=1;
     countEl.innerText = count;
-}
+})
 
-function save() {
+saveBtn.addEventListener("click", function(){
     saveEl.innerText += count + " - ";
     count = 0;
     countEl.innerText = 0;
-}
+})
